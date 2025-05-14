1865a754c45c8ceed7c7c7a5136c0e7da68be187
Bautin Stepan bc2024-6 <stepanbaytun@gmail.com>
2025-05-14 23:57:32 +0300

    Installed and setup react-toastify

--- cinema_lab/src/App.jsx
+++ cinema_lab/src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import Home from './pages/Home';
 import Booking from './pages/Booking';
 import './css/MovieList.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -16,6 +18,15 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/booking/:id" element={<Booking />} />
         </Routes>
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          closeOnClick
+          pauseOnHover
+          draggable
+          theme="light"
+        />
       </div>
     </Router>
   );
